262c33eb7ba3bb6937bf9d41ccfa1c5c0f2c82cd
Aman <AMANROY9658@GMAIL.COM>
2026-04-05 13:44:07 +0530

    role authorization

---
+++ src/middleware/role.middleware.js
@@ -0,0 +1,15 @@
+export const allowRoles = (...allowedRoles) => {
+    return (req, res, next)=>{
+        const user =req.user;
+
+        if (!user){
+            return res.status(401).json({message: "Unauthorized"});
+        }
+
+        if (!allowedRoles.includes(user.role)){
+            return res.status(403).json({message: "Forbidden"});
+        }
+
+        next();
+    }};
+
