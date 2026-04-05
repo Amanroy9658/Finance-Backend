7978a6d48a3fea3f654d05fbd7ea348f704e24fb
Aman <AMANROY9658@GMAIL.COM>
2026-04-05 16:28:55 +0530

    transaction model

---
+++ src/modules/transaction/transaction.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+    amount: {
+        type:number,
+        required:true,
+        min: 0,
+    },
+   type:{
+    type:String,
+    enum:["income", "expense"],
+    required: true,
+    index: true,
+   },
+   category: {
+    type: String,
+    required:true,
+    index:true,
+   
+
+   },
+
+   date: {
+    type:Date,
+    required:true,
+    index:true,
+   },
+   note: {
+    type:String,
+    trim:true,
+   },
+   createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    reequired:true,
+   },
+
+},
+{
+    timestamps: true,
+}
+
+);
+
+export const Transaction = mongoose.model("Transaction", transactionSchema);
