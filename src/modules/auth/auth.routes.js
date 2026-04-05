b5b9b454c4f8c96463b377f0c873c56786245d66
Aman <AMANROY9658@GMAIL.COM>
2026-04-05 17:46:01 +0530

    protectedroute added

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { loginController } from "./auth.controller.js";
-
+import { protect } from "../../middleware/auth.middleware.js";
+import { allowRoles } from "../../middleware/role.middleware.js";
 const router = express.Router();
 
 // Public route (no auth required)
 router.post("/login", loginController);
-
+router.get("/", protect, allowRoles("admin"),...);
 export default router;
